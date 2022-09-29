314efbb2e7ffd72b7222b734f90ccf812ea00215
Elaine Rose Miculob <elainerose.miculob@gmail.com>
2022-09-29 12:06:07 +0800

    Added all files for miniproject1

--- js/script.js
+++ js/script.js
@@ -5,21 +5,3 @@ menuBtn.onclick = () => {
   menuBtn.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
-
-var swiper = new Swiper(".home-slider", {
-  effect: "coverflow",
-  loop: true,
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
